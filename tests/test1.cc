ad9d25d8816e8b623202a1bdb4238a8e75d5474a
rpeschke <peschke@hawaii.edu>
2025-07-20 13:38:45 +0200

    removed old code

--- tests/test1.cc
+++ tests/test1.cc
@@ -67,131 +67,6 @@ void my_function(ARGS &&...args)
   std::cout << t0 << std::endl;
 }
 
-template <typename FIELD_T, typename VECT_T,  std::size_t Index>
-struct member_span
-{
-  member_span()
-  {
-  }
-  static constexpr std::size_t field_index = Index;
-  using M_FIELD_T = FIELD_T;
-  using M_VEC_T = VECT_T;
-
-  nt::span<FIELD_T> get()
-  {
-    auto & m_vec =  reinterpret_cast<VECT_T&>(* (this  - field_index) );
-      
-
-      
-    return nt::span<FIELD_T>(                   
-    m_vec.m_data.size() ? (std::byte *)& (  FIELD_T::get(m_vec.m_data[0])) : nullptr, 
-    m_vec.m_data.size(), sizeof(decltype(m_vec.m_data[0])));
-
-
-
-  }
-
-  
-  auto get_primitive()
-  {
-    auto & m_vec =  reinterpret_cast<VECT_T&>(* (this  - field_index) );
-      
-      
-      
-    return nt::span<decltype(FIELD_T::get(m_vec.m_data[0]).v) >(                   
-    m_vec.m_data.size() ? (std::byte *)& (  FIELD_T::get(m_vec.m_data[0])) : nullptr, 
-    m_vec.m_data.size(), sizeof(decltype(m_vec.m_data[0])));
-
-
-
-  }
-
-
-  auto operator()(){
-    return get();
-  }
-
-};
-
-template <typename IndexSeq, typename... Ts>
-struct vector_frame_impl;
-
-template <std::size_t... Is, typename... Ts>
-struct vector_frame_impl<std::index_sequence<Is...>, Ts...> :
-
-          nt::base_maker_t<
-                          nt::_Remove_cvref_t<Ts>, 
-                          nt::ax_type2<
-                                      member_span<Ts, vector_frame_impl<std::index_sequence<Is...>, Ts...>, Is>,
-                                      typename Ts::struct_maker
-                                      >
-                            >...
-{
-   std::vector<nt::ntuple<Ts...>> m_data;
-  /* data */
-  vector_frame_impl(const std::vector<
-          nt::ntuple<Ts...>>&  vec) : m_data(vec) {}
-
-    vector_frame_impl() =default;
-friend auto begin(vector_frame_impl const& v) { return v.m_data.begin(); }
-friend auto end(vector_frame_impl const& v) { return v.m_data.end(); }
-
-friend auto begin(vector_frame_impl & v) { return v.m_data.begin(); }
-friend auto end(vector_frame_impl & v) { return v.m_data.end(); }
-friend std::ostream &operator<<(std::ostream &os, const vector_frame_impl & v) {
-    os << v.m_data;
-    return os;
-}
-  decltype(auto) operator[](std::size_t i){
-    return m_data[i];
-  }
-  decltype(auto) operator[](std::size_t i) const{
-    return m_data[i];
-  }
-};
-
-
-template <typename... Ts>
-using vector_frame = vector_frame_impl<std::index_sequence_for<Ts...>, Ts...>;
-
-
-
-template <typename... T> 
-auto to_vector_frame(const  std::vector< nt::ntuple< T... > > & vec ){
-  return vector_frame<T...>(vec);
-}
-
-template <typename... T> 
-auto to_vector_frame1(const   nt::ntuple< T... >  & vec ){
-  return vector_frame<T...>(std::vector<nt::ntuple< T... >> {});
-}
-
-
-  template <typename FUNC_T>
-  auto fill_vector_frame(size_t entries, FUNC_T &&func)
-  {
-    decltype(to_vector_frame1(func(size_t(0)))) ret;
-    ret.m_data.reserve(entries);
-    for (size_t i = 0; i < entries; ++i)
-    {
-      ret.m_data.push_back(func(i));
-    }
-    return ret;
-  }
-
-   template <typename RANGE_T>
-  auto fill_vector_frame(RANGE_T && range_)
-  {
-
-    decltype(to_vector_frame1( *range_.begin() )) ret;
-    //ret.m_data.reserve(entries);
-    for (auto&& e : range_)
-    {
-      ret.m_data.push_back(e);
-    }
-    return ret;
-  }
-
 template <typename T>
 void fun(nt::span<T> sp)
 {
@@ -249,39 +124,42 @@ int main(int argv, char **argc)
                         { return nt::ntuple(nt_field(timestwo) = e.index * 2.12); });
   std::cout << df1;
 
-
-
-  //auto v = vector_frame<decltype(index = 1), decltype(index_squared = 1)>{df};
+  // auto v = vector_frame<decltype(index = 1), decltype(index_squared = 1)>{df};
   auto v = nt::to_vector_frame(df1);
   std::cout << (uint64_t)&v << std::endl;
-  for( auto &&e : v.index.get_primitive() ){
+  for (auto &&e : v.index.get_primitive())
+  {
 
     std::cout << e << std::endl;
   }
 
-  for( auto &&e : v.timestwo.get_primitive() ){
+  for (auto &&e : v.timestwo.get_primitive())
+  {
 
     std::cout << e << std::endl;
   }
 
   using index_t = decltype(index = 3);
 
-
   // auto vec = vector_frame<decltype(df1[0].timestwo), typename decltype(df1[0].timestwo)::template  struct_maker<int> >{};
-  auto df2 = fill_vector_frame(10, [](auto i)
-                                        { return nt::ntuple(
-                                              index = i,
-                                              index_squared = i * i); });
+  auto df2 = nt::fill_vector_frame(10, [](auto i)
+                                   { return nt::ntuple(
+                                         index = i,
+                                         index_squared = i * i); });
 
-            std::cout << df2.m_data << std::endl;          
-            
-  auto df3 = nt::fill_vector_frame(std::views::iota(1, 10) | std::views::transform([](auto i)
-                                                                  { return nt::ntuple(index = i, index_squared = i * i, nt_field(cubed) = i * i * i); }) );
+  std::cout << df2 << std::endl;
 
-  std::cout << df3[0] << std::endl;          
-  for( auto &&e : df3 ){
+  auto df3 = nt::fill_vector_frame(std::views::iota(1, 10) 
+                                      | std::views::transform([](auto i) { 
+                                        return nt::ntuple(index = i, index_squared = i * i, nt_field(cubed) = i * i * i); 
+                                      })
+                                  );
+
+  std::cout << df3[0] << std::endl;
+  for (auto &&e : df3)
+  {
     e.cubed.v += 2;
-    std::cout << e.cubed  << std::endl;
+    std::cout << e.cubed << std::endl;
   }
   return 0;
 }
