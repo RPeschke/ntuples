9388dcad725c56eb35a88bc071c0ff6008facded
rpeschke <peschke@hawaii.edu>
2025-08-15 11:41:21 +0200

    trying to work with vcpkg

--- cmake/ntuples-config.cmake
+++ cmake/ntuples-config.cmake
@@ -1,7 +1,21 @@
-include("${CMAKE_CURRENT_LIST_DIR}/ntuples-targets.cmake")
+# cmake/ntuples-config.cmake
+# Minimal, relocatable config for a header-only INTERFACE target.
 
 if (TARGET ntuples::ntuples)
   return()
 endif()
 
 add_library(ntuples::ntuples INTERFACE IMPORTED)
+
+# Point to the installed include/ relative to this config file.
+# In vcpkg, this file ends up in: <installed>/share/ntuples/ntuples-config.cmake
+# So include dir is two levels up, then /include.
+get_filename_component(_IMPORT_PREFIX "${CMAKE_CURRENT_LIST_DIR}/../.." ABSOLUTE)
+
+target_include_directories(ntuples::ntuples
+  INTERFACE
+    "${_IMPORT_PREFIX}/include"
+)
+
+# Optional: if you require a certain standard
+# target_compile_features(ntuples::ntuples INTERFACE cxx_std_20)
